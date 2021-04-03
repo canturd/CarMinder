0aafa71c90e0927b00de01a84f0d09a12a1b5f2f
Karen Dan <liendanld@gmail.com>
2021-04-02 20:38:11 -0700

    I added the popup activity for each button, and display just the name of the service in the popup screen

--- app/src/main/java/com/firstapp/carminder/MainActivity.java
+++ app/src/main/java/com/firstapp/carminder/MainActivity.java
@@ -3,7 +3,9 @@ package com.firstapp.carminder;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_scheduler);
+    }
+
+    public void launch_serviceSchedule(View view) {
+        Intent serviceScheduler = new Intent(this, serviceScheduler.class);
+        startActivity(serviceScheduler);
     }
 }
 
---
+++ app/src/main/java/com/firstapp/carminder/popup.java
@@ -0,0 +1,31 @@
+package com.firstapp.carminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
+
+public class popup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup);
+
+        //get default activity screen size
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        //set new screen size for popup
+        getWindow().setLayout((int)(width*0.8), (int)(height*0.6));
+
+        Intent intent = getIntent();
+        String serviceName = intent.getStringExtra("SERVICE_NAME");
+        ((TextView)findViewById(R.id.serviceName)).setText(serviceName);
+
+    }
+}
