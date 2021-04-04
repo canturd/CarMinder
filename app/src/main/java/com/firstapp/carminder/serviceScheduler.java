c1e5c98b1801c7971cf47a0ca695363a4b1a8148
canturd <canterduran@yahoo.com>
2021-04-04 12:07:26 -0700

    Updated background image and added onClick and scheduler methods to main

--- app/src/main/java/com/firstapp/carminder/serviceScheduler.java
+++ app/src/main/java/com/firstapp/carminder/serviceScheduler.java
@@ -3,6 +3,7 @@ package com.firstapp.carminder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class serviceScheduler extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class serviceScheduler extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_scheduler);
+    }
+
+    public void onClick(View view) {
+        findViewById(R.id.brakes_button);
+
     }
 }
