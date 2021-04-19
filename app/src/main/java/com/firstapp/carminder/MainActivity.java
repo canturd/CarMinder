147f8c1dbe1ef549f7a89b9867a16b060a69d6e1
canturd <70295940+canturd@users.noreply.github.com>
2021-04-19 13:50:18 -0700

    Added drop down to addcar menu. added car make and year string arrays to strings.xml

--- app/src/main/java/com/firstapp/carminder/MainActivity.java
+++ app/src/main/java/com/firstapp/carminder/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageButton;
 
--- app/src/main/java/com/firstapp/carminder/add_car.java
+++ app/src/main/java/com/firstapp/carminder/add_car.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class add_car extends AppCompatActivity {
 
@@ -11,6 +13,21 @@ public class add_car extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
+
+        //set up drop down of makes
+        String[] makes = getResources().getStringArray(R.array.makes);
+        AutoCompleteTextView editText = findViewById(R.id.car_make);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, makes);
+        editText.setAdapter(adapter);
+
+        //set up drop down of years
+        String[] years = getResources().getStringArray(R.array.year);
+        AutoCompleteTextView editText1 = findViewById(R.id.car_year);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, years);
+        editText1.setAdapter(adapter1);
+
+        //how to get input out of drop down
+        //String input = getText.getText().toString();
     }
 
     public void cancel_activity(View view) {
